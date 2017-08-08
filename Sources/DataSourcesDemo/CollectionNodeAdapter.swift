b2a38a834a7dbe53b4964be73ef533f8a806c00f
muukii <m@muukii.me>
2017-08-08 17:17:31 +0900

    Add CollectionNodeAdapter

---
+++ Sources/DataSourcesDemo/CollectionNodeAdapter.swift
@@ -0,0 +1,88 @@
+//
+//  CollectionNodeAdapter.swift
+//  DataSourcesDemo
+//
+//  Created by muukii on 8/8/17.
+//  Copyright © 2017 muukii. All rights reserved.
+//
+
+import Foundation
+
+import DataSources
+import class AsyncDisplayKit.ASCollectionNode
+
+public final class CollectionNodeUpdater: Updating {
+
+  private(set) public weak var collectionNode: ASCollectionNode?
+
+  public init(collectionNode: ASCollectionNode) {
+    self.collectionNode = collectionNode
+  }
+
+  public func insertItems(at indexPaths: [IndexPath]) {
+
+    guard let collectionNode = collectionNode else {
+      assertionFailure("collectionNode has released")
+      return
+    }
+
+    collectionNode.insertItems(at: indexPaths)
+  }
+
+  public func deleteItems(at indexPaths: [IndexPath]) {
+
+    guard let collectionNode = collectionNode else {
+      assertionFailure("collectionNode has released")
+      return
+    }
+
+    collectionNode.deleteItems(at: indexPaths)
+  }
+
+  public func reloadItems(at indexPaths: [IndexPath]) {
+
+    guard let collectionNode = collectionNode else {
+      assertionFailure("collectionNode has released")
+      return
+    }
+
+    collectionNode.reloadItems(at: indexPaths)
+  }
+
+  public func moveItem(at indexPath: IndexPath, to newIndexPath: IndexPath) {
+
+    guard let collectionNode = collectionNode else {
+      assertionFailure("collectionNode has released")
+      return
+    }
+
+    collectionNode.moveItem(at: indexPath, to: newIndexPath)
+  }
+
+  public func performBatch(updates: () -> Void, completion: @escaping () -> Void) {
+
+    guard let collectionNode = collectionNode else {
+      assertionFailure("collectionNode has released")
+      return
+    }
+
+    collectionNode.performBatchUpdates({
+      updates()
+    }, completion: { result in
+      completion()
+    })
+
+  }
+
+  public func reload(completion: @escaping () -> Void) {
+
+    guard let collectionNode = collectionNode else {
+      assertionFailure("collectionNode has released")
+      return
+    }
+
+    collectionNode.reloadData {
+      completion()
+    }
+  }
+}
