90a7f50e5e51abd1f61d3ef2f07c47a50e9454f3
muukii <m@muukii.me>
2017-08-21 00:34:03 +0900

    Fix import

--- Sources/DataSources/CollectionViewDataSource.swift
+++ Sources/DataSources/CollectionViewDataSource.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2017 muukii. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 public final class CollectionViewDataSource<T: Diffable> : NSObject, UICollectionViewDataSource {
 
